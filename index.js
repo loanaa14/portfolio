939617ab3cc7421303fc969b2f938aa7bb8f3272
SheilaMart <sheilamartinez372@gmail.com>
2026-01-04 14:55:17 -0300

    Algunos cambios y redirecciones

---
+++ index.js
@@ -0,0 +1,27 @@
+const links = document.querySelectorAll('.nav-links a');
+const paginas = document.querySelectorAll('.pagina');
+const hero = document.querySelector('.hero');
+
+links.forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        
+        const id = link.href.split('#')[1];
+
+        if (id === "inicio") {
+            hero.classList.remove('oculto');
+            paginas.forEach(pagina => {
+                pagina.classList.remove('activa');
+            });
+            return;
+        }
+
+        hero.classList.add('oculto');
+        
+        paginas.forEach(pagina => {
+            pagina.classList.remove('activa');
+        });
+
+        document.getElementById(id).classList.add('activa');
+    });
+});
