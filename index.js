bc4fef8fe8cd639cc24073b5872e977e816198e5
Loana Leguizamon <loanaleguizamon1@gmail.com>
2026-01-08 17:00:02 -0300

    tarjetas-formulario

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const links = document.querySelectorAll('.nav-links a');
 const paginas = document.querySelectorAll('.pagina');
 const hero = document.querySelector('.hero');
 
+
 links.forEach(link => {
     link.addEventListener('click', e => {
         e.preventDefault();
-        
+       
         const id = link.href.split('#')[1];
+
 
         if (id === "inicio") {
             hero.classList.remove('oculto');
@@ -16,11 +18,13 @@ links.forEach(link => {
             return;
         }
 
+
         hero.classList.add('oculto');
-        
+       
         paginas.forEach(pagina => {
             pagina.classList.remove('activa');
         });
+
 
         document.getElementById(id).classList.add('activa');
     });
